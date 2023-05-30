59e677bfb052056a36220c77c568af340cfe17c9
Hyeun9991 <hyeun9991@gmail.com>
2023-05-30 17:09:44 +0900

    register post 요청 처리

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
+
+const { User } = require('./models/user');
 
 const mongoURL = process.env.MONGO_URL;
 const mongoose = require('mongoose');
@@ -15,8 +18,24 @@ mongoose
     console.error(err);
   });
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.use(express.json()); // JSON 데이터를 파싱하기 위한 미들웨어
+app.use(express.urlencoded({ extended: true })); // URL 인코딩된 데이터를 파싱하기 위한 미들웨어
+app.use(cookieParser());
+
+app.post('/api/users/register', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res
+      .status(201)
+      .json({ success: true, message: 'User registered successfully' });
+  } catch (err) {
+    let errorMessage = 'Unable to register user';
+    if (err.code === 11000 && err.keyPattern.email === 1) {
+      errorMessage = 'Email already exists';
+    }
+    res.status(400).json({ success: false, message: errorMessage });
+  }
 });
 
 const port = process.env.PORT || 8080;
