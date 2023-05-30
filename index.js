adb7d63bd89f72a34a15fc68bd6d8a4a5fb6c135
Hyeun9991 <hyeun9991@gmail.com>
2023-05-30 16:20:57 +0900

    mongoose를 사용하여 Node.js 애플리케이션과 MongoDB를 연결

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+
+const mongoURL = process.env.MONGO_URL;
+const mongoose = require('mongoose');
+mongoose
+  .connect(mongoURL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('DB connected');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(8080);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('heurm server is listening to port ' + port);
+});
